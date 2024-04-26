39202b6aae20c8d86dfcb400ebdcf5d8b3d78377
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2024-04-26 01:58:29 -0400

    fix on hashing the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import bcrypt from 'bcrypt';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
-    console.log(dto)
+    console.log(dto.password);
     //hash
-    const hash = await bcrypt.hash(dto.password, 10);
-    console.log(hash);
+    const hashPassword = await hash(dto.password, 8);
     const user = await this.prisma.user.create({
       data: {
         email: dto.email,
-        hash,
+        hash: hashPassword,
       },
     });
     return user;
